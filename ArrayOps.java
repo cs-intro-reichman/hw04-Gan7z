37b4b90bdc26afcc7a2bfe072f49d3d71475f61a
Gan7z <oshri.gantz@post.runi.ac.il>
2024-01-13 21:25:46 +0200

    v0.0.1

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,120 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] array1 = {1,-1};
+        System.out.println((isSorted(array1)));
     }
-    
+    /**
+     * The function receive an array of integers values
+     * The array must contain each integers between 0 <= x <= n | when n is the length of the array
+     * Except for a single integer, the function returns that missing integer value.
+     */
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int sequenceSum = 0;
+        int arraySum = 0;
+        for(int i = 0; i <= array.length; i++){
+            sequenceSum += i;
+        }
+        for(int i = 0; i < array.length; i++){
+            arraySum += array[i];
+        }
+        int answer = sequenceSum - arraySum;
+        if (answer == 0){
+            return 0;
+        }
+        else{
+            return answer;
+        }
     }
 
+    /**
+     * This function takes an array of integers and returns the second largest number in the array.
+     * The numbers in the array may appear more than once. We Assume that the array’s length is  >= 1.
+     * If the number appears twice and it is the largest number, it is also the 2nd largest number. 
+     */
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int maxValue = Math.max(array[0], array[1]);
+        int secondMaxValue = Math.min(array[0], array[1]);
+        for (int i = 2; i < array.length;i++){
+            if (array[i] > secondMaxValue){
+                if (array[i]> maxValue){
+                    secondMaxValue = maxValue;
+                    maxValue = array[i];
+                }
+                else{
+                    secondMaxValue = array[i];
+                }
+            }
+        }
+        return secondMaxValue;
     }
 
+    /**
+     * This function takes two arrays of integers, and returns true if both arrays contain the same numbers.
+     * The number of occurrences of each number, and their order, are not important
+     */
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        /*
+         * We need to check that array1 is contained in array2 and the vice versa
+         * If we found the integer in one of the arrays in the other, we can skip to the next number instead of continuing the search.
+         * If we reached the end of the second array, and we didn't find the values, we know they do not contain the same numbers.
+         */
+        for(int i = 0; i < array1.length; i++){ 
+            for(int j = 0; j < array2.length; j++){
+                if (array1[i] == array2[j]){
+                    break;
+                } 
+                if (j == array2.length-1){
+                    return false;
+                }
+            }
+        }
+        for(int i = 0; i < array2.length; i++){
+            for(int j = 0; j < array1.length; j++){
+                if (array2[i] == array1[j]){
+                    break;
+                } 
+                if (j == array1.length-1){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
+    /**
+     * This function takes an array of integers and checks if the array is sorted increasingly, from the minimum value to the maximum value.
+     * Or decreasingly, from the maximum value to the minimum value.
+     */
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
-    }
+        /*
+         * Extreme case, we need to take into consideration if there isn't a change in value in following neighboor integers
+         * Example: {1, 1, 1,1,1,1,1,0,-1,-3,5}
+         * so we need to find the index where there is a change in value
+         * For efficency we should start checking for ascending or descending order from that specific index, instead from the beginning of the array
+         */
+        int startIndex = 0;
+        for(int i = 0; i < array.length-1; i++){
+            if (array[i] != array[i+1]){
+                startIndex=i;
+                break;
+            }
+            if (i == (array.length-2)){
+                return true;
+            }
+        }
 
+        boolean check = array[startIndex] > array[startIndex+1] ? true : false; // Is the array in ascending order or descending?
+        for(int i = startIndex; i < array.length-1; i++){
+            if (check == false){ // Ascending
+                if (array[i+1] < array[i]){
+                    return false;
+                }
+            } else {
+                if (array[i+1] > array[i]){ // Descending
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -22,21 +24,98 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println("You are currently not testing any function :)\nPlease change the main method");
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String temp = "";
+        for (int i = 0; i < string.length(); i++){
+            switch (string.charAt(i)) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    temp += (char)(string.charAt(i) - 32);
+                    break;
+                default:
+                    if (string.charAt(i) > 64 && string.charAt(i) < 91){
+                        temp += (char)(string.charAt(i) + 32);
+                    } else {
+                        temp += string.charAt(i);
+                    }
+                    break;
+            }
+        }
+        return temp;
     }
 
+    /**
+     * This function takes as an input a string containing only letters, organized into words that are separated with a single space
+     * The function returns a string in which the original string is modified according to three rules:
+     *  1. The first word should be in lowercase.
+     *  2. Every word should start with an uppercase letter, and all the remaining letters in the word should be lowercase.
+     *  3. All the spaces should be removed.
+     */
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String answer = "";
+        for (int i = 0; i < string.length();i++){
+            if (string.charAt(i) == ' ' && answer==""){ // removing spaces in the beginning of a sentence
+                continue;
+            }
+            else if (string.charAt(i) == ' '){ // removing spaces in the rest of the sentence
+                continue;
+            }
+            else if (answer == ""){ //special case, first letter of the sentence
+                if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+                    answer += (char)(string.charAt(i)+32);
+                }
+                else{
+                    answer += string.charAt(i);
+                }
+            }
+            else{
+                if (string.charAt(i-1) == ' '){ // first letter of word
+                    if (string.charAt(i) >= 'a' && string.charAt(i) <= 'z'){
+                        answer += (char)(string.charAt(i)-32);
+                    }
+                    else{
+                        answer += string.charAt(i);
+                    }
+                }
+                else{
+                    if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+                        answer += (char)(string.charAt(i)+32);
+                    }
+                    else{
+                        answer += string.charAt(i);
+                    }
+                }
+            }
+        }
+        return answer;
     }
 
+    /**
+     * This function receives two parameters: A string and a character.
+     * The function returns an array containing all the indices in which the character appears in the string.
+     * We assume that the given string is not empty, and that the given character appears in the string at least once.
+     */
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int[] temp = new int[string.length()]; // temporary unefficent array
+        int numOfCharAppearance = 0; // used to create efficent array length
+        int tempIndex = 0;
+        for (int i = 0; i < string.length();i++){
+            if (string.charAt(i) == chr){
+                numOfCharAppearance++;
+                temp[tempIndex] += string.indexOf(chr, i);
+                tempIndex++;
+            }
+        }
+        int[] answer = new int[numOfCharAppearance];
+        for (int i = 0; i < answer.length; i++){ 
+            answer[i] = temp[i];
+        }
+        return answer;
     }
 }
